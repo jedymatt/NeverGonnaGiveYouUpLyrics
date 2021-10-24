85eed12edde4e008763ea7829f6bff583f7ac888
Jedy Matt Tabasco <jedymatt@gmail.com>
2021-10-24 19:01:25 +0800

    Added display image and lyrics

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,114 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const App());
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: ('Never Gonna Give You Up Lyrics'),
+      theme: ThemeData.light(),
+      home: const HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        title: const Center(
+          child: Text('Never Gonna Give You Up Lyrics'),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: const BodyContent(),
+    );
+  }
+}
+
+class BodyContent extends StatelessWidget {
+  const BodyContent({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          padding: const EdgeInsets.only(left: 10, right: 10, top: 10),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(8),
+            child: const Image(
+              image: AssetImage('assets/rickroll_4k.jpg'),
+            ),
+          ),
+        ),
+        const Expanded(
+          child: SongLyrics(),
+        ),
+      ],
+    );
+  }
+}
+
+class SongLyrics extends StatelessWidget {
+  const SongLyrics({Key? key}) : super(key: key);
+
+  final lyrics = '''
+  We’re no strangers to love
+  You know the rules and so do I
+  A full commitment’s what I’m thinking of
+  You wouldn’t get this from any other guy
+
+  I just wanna tell you how I’m feeling
+  Gotta make you understand
+
+  Never gonna give you up
+  Never gonna let you down
+  Never gonna run around and desert you
+  Never gonna make you cry
+  Never gonna say goodbye
+  Never gonna tell a lie and hurt you
+
+  We’ve known each other for so long
+  Your heart’s been aching but you’re too shy to say it
+  Inside we both know what’s been going on
+  We know the game and we’re gonna play it
+
+  And if you ask me how I’m feeling
+  Don’t tell me you’re too blind to see
+
+  Never gonna give you up
+  Never gonna let you down
+  Never gonna run around and desert you
+  Never gonna make you cry
+  Never gonna say goodbye
+  Never gonna tell a lie and hurt you
+  ''';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scrollbar(
+      interactive: true,
+      child: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.only(left: 10, right: 10),
+          margin: const EdgeInsets.only(top: 10, bottom: 10),
+          child: Text(
+            lyrics,
+            textAlign: TextAlign.center,
+            textScaleFactor: 1.2,
+            style: const TextStyle(
+              height: 1.4,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
